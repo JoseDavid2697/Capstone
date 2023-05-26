aebf4250369e88c0a2d6a0d9e730aac016b57a0a
JoseDavid2697 <37557565+JoseDavid2697@users.noreply.github.com>
2023-05-26 17:27:18 -0600

    Add files via upload

--- game.py
+++ game.py
@@ -22,9 +22,10 @@ level = 1
 jump_counter = 0
 game_over = 0
 score = 0
+score_silver = 0
 background = 'img/sky.png'
 last_level = False
-
+player_img = 'kirby'
 white = (255, 255, 255)
 
 #load images
@@ -33,6 +34,15 @@ bg_img = pygame.image.load(background)
 start_img = pygame.image.load('img/start_btn.png')
 exit_img = pygame.image.load('img/exit_btn.png')
 post_img = pygame.image.load('img/post_btn.png')
+ed_img = pygame.image.load('img/ed_btn.png')
+juan_img = pygame.image.load('img/juan_btn.png')
+ana_img = pygame.image.load('img/ana_btn.png')
+ethan_img = pygame.image.load('img/ethan_btn.png')
+vicky_img = pygame.image.load('img/vicky_btn.png')
+camilo_img = pygame.image.load('img/camilo_btn.png')
+victor_img = pygame.image.load('img/victor_btn.png')
+johan_img = pygame.image.load('img/johan_btn.png')
+kirb_img = pygame.image.load('img/kirb_btn.png')
 
 def set_level():
 	global bg_img
@@ -71,12 +81,17 @@ def draw_text(text, font, text_col, x, y):
 	img = font.render(text, True, text_col)
 	screen.blit(img, (x, y))
 
+def draw_text_silver(text, font, text_col, x, y):
+	img = font.render(text, True, text_col)
+	screen.blit(img, (x, y))
+
 #function to reset level
 def reset_level(level):
 	player.reset(100, screen_height - 130)
 	exit_group.empty()
 	if level == 3:
 		coin_group.empty()
+		silver_group.empty()
 		world_data = world_data = set_level()
 		world = World(world_data)
 	else:
@@ -86,6 +101,7 @@ def reset_level(level):
 
 	if level == 3:
 		score = 0
+		score_silver = 0
 
 	return world
 
@@ -120,13 +136,14 @@ class Button():
 		return action
 
 class Player():
-	def __init__(self, x, y):
+	def __init__(self, x, y, img):
 		self.images_right = []
 		self.images_left = []
 		self.index = 0
 		self.counter = 0
+		self.player_img = img
 		for num in range(1, 5):
-			img_right = pygame.image.load(f'img/guy{num}.png')
+			img_right = pygame.image.load(f'img/{self.player_img}{num}.png')
 			img_right = pygame.transform.scale(img_right, (40, 80))
 			img_left = pygame.transform.flip(img_right, True, False)
 			self.images_right.append(img_right)
@@ -219,7 +236,7 @@ class Player():
 			if pygame.sprite.spritecollide(self, furnace_group, False):
 				if post_button.draw():
 					#Make post method
-					print('Collide with furnance')
+					print('Click')
 
 			#update player coordinates
 			self.rect.x += dx
@@ -241,7 +258,7 @@ class Player():
 		self.index = 0
 		self.counter = 0
 		for num in range(1, 5):
-			img_right = pygame.image.load(f'img/guy{num}.png')
+			img_right = pygame.image.load(f'img/{self.player_img}{num}.png')
 			img_right = pygame.transform.scale(img_right, (40, 80))
 			img_left = pygame.transform.flip(img_right, True, False)
 			self.images_right.append(img_right)
@@ -288,6 +305,9 @@ class World():
 				if tile == 7:
 					coin = Coin(col_count * tile_size + (tile_size // 2), row_count * tile_size + (tile_size // 2))
 					coin_group.add(coin)
+				if tile == 9:
+					silver = Silver(col_count * tile_size + (tile_size // 2), row_count * tile_size + (tile_size // 2))
+					silver_group.add(silver)
 				if tile == 8:
 					exit = Exit(col_count * tile_size, row_count * tile_size - (tile_size // 2))
 					exit_group.add(exit)
@@ -310,6 +330,13 @@ class Coin(pygame.sprite.Sprite):
 		self.rect = self.image.get_rect()
 		self.rect.center = (x, y)
 
+class Silver(pygame.sprite.Sprite):
+	def __init__(self, x, y):
+		pygame.sprite.Sprite.__init__(self)
+		img = pygame.image.load('img/silver.png')
+		self.image = pygame.transform.scale(img, (tile_size // 2, tile_size // 2))
+		self.rect = self.image.get_rect()
+		self.rect.center = (x, y)
 
 class Exit(pygame.sprite.Sprite):
 	def __init__(self, x, y):
@@ -329,16 +356,26 @@ class Furnace(pygame.sprite.Sprite):
 		self.rect.x = x
 		self.rect.y = y
 
-player = Player(100, screen_height - 130)
+#player = Player(100, screen_height - 130, player_img)
 
 exit_group = pygame.sprite.Group()
 coin_group = pygame.sprite.Group()
+silver_group = pygame.sprite.Group()
 furnace_group = pygame.sprite.Group()
 
 #create buttons
-start_button = Button(screen_width // 2 - 350, screen_height // 2, start_img)
-exit_button = Button(screen_width // 2 + 150, screen_height // 2, exit_img)
-post_button = Button(screen_width // 2 + 150, screen_height // 2, post_img)
+#start_button = Button(screen_width // 2 - 350, screen_height // 2, start_img)
+exit_button = Button(screen_width // 2 + 150, screen_height // 2 + 50, exit_img)
+post_button = Button(screen_width // 2 - 150, screen_height // 2, post_img)
+ed_button = Button(screen_width // 2 - 350, screen_height // 100 + 700, ed_img)
+juan_button = Button(screen_width // 2 - 250, screen_height // 100 + 700, juan_img)
+ana_button = Button(screen_width // 2 - 150, screen_height // 100 + 700, ana_img)
+ethan_button = Button(screen_width // 2 - 50, screen_height // 100 + 700, ethan_img)
+vicky_button = Button(screen_width // 2 - 350, screen_height // 100 + 600, vicky_img)
+camilo_button = Button(screen_width // 2 - 350, screen_height // 2, camilo_img)
+johan_button = Button(screen_width // 2 - 250, screen_height // 2, johan_img)
+victor_button = Button(screen_width // 2 - 150, screen_height // 2, victor_img)
+kirb_button = Button(screen_width // 2 - 50, screen_height // 2 , kirb_img)
 #load in level data and create world
 pickle_in = open(f'level{level}_data', 'rb')
 world_data = pickle.load(pickle_in)
@@ -346,7 +383,6 @@ world = World(world_data)
 
 run = True
 while run:
-
 	clock.tick(fps)
 
 	if last_level == False:
@@ -359,18 +395,50 @@ while run:
 	if main_menu == True:
 		if exit_button.draw():
 			run = False
-		if start_button.draw():
+		#if start_button.draw():
+			#main_menu = False
+		if ed_button.draw():
 			main_menu = False
+			player.player_img = 'ed'
+		if juan_button.draw():
+			main_menu = False
+			player_img = 'guy'
+		if ana_button.draw():
+			main_menu = False
+			player.player_img = 'ana'
+		if ethan_button.draw():
+			main_menu = False
+			player.player_img = 'ethan'
+		if vicky_button.draw():
+			main_menu = False
+			player.player_img = 'vicky'
+		if victor_button.draw():
+			main_menu = False
+			player.player_img = 'victor'
+		if camilo_button.draw():
+			main_menu = False
+			player.player_img = 'camilo'
+		if johan_button.draw():
+			main_menu = False
+			player.player_img = 'johan'
+		if kirb_button.draw():
+			main_menu = False
+			player_img = 'kirby'	
+		player = Player(100, screen_height - 130, player_img)
 	else:
 		world.draw()
 
 		if pygame.sprite.spritecollide(player, coin_group, True):
 			score += 1
+		if pygame.sprite.spritecollide(player, silver_group, True):
+			score_silver += 1
 
 		draw_text('Gold: ' + str(score), font_score, white, tile_size - 10, 10)
+		draw_text('Silver: ' + str(score_silver), font_score, white, tile_size + 120, 10)
 
 		exit_group.draw(screen)
 		coin_group.draw(screen)
+		silver_group.draw(screen)
 		furnace_group.draw(screen)
 		game_over = player.update(game_over)
 
